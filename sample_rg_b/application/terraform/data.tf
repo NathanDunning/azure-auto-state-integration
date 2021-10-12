92b9546c8c64cc630d9867bab40adf273db6f644
Nathan Dunning <Nathan.Dunning@acc.co.nz>
2021-10-13 09:28:44 +1300

    Add application for rg b

---
+++ sample_rg_b/application/terraform/data.tf
@@ -0,0 +1,19 @@
+data "azurerm_resource_group" "main" {
+  name = "SAMPLE_B_RG"
+}
+
+data "azurerm_virtual_network" "main" {
+  name                = "virtualNetwork1"
+  resource_group_name = data.azurerm_resource_group.main.name
+}
+
+data "azurerm_subnet" "main" {
+  name                 = "subnet1"
+  virtual_network_name = data.azurerm_virtual_network.main.name
+  resource_group_name  = data.azurerm_resource_group.main.name
+}
+
+data "azurerm_availability_set" "main" {
+  name                = "sample-bset"
+  resource_group_name = data.azurerm_resource_group.main.name
+}
---
+++ sample_rg_b/application/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "admin_username" {
+  type        = string
+  description = "Admin username"
+  sensitive   = true
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Admin password"
+  sensitive   = true
+}
