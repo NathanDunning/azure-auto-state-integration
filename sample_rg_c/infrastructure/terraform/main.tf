3d4a14cec091cd818bfb2a9f87ded95607f48268
Nathan Dunning <dunningn@gmail.com>
2021-10-17 22:42:26 +1300

    Updating resource group c values for script matching

--- sample_rg_c/infrastructure/terraform/main.tf
+++ sample_rg_c/infrastructure/terraform/main.tf
@@ -1,17 +1,17 @@
 resource "azurerm_resource_group" "main" {
   location = "westus"
-  name     = "SAMPLE_C_RG"
+  name     = var.resource_group_name
 }
 
 resource "azurerm_virtual_network" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
-  name          = "virtualNetwork1"
+  name          = var.virtual_network_name
   address_space = ["10.0.0.0/16"]
 
   subnet {
-    name           = "subnet1"
+    name           = var.subnet_name
     address_prefix = "10.0.1.0/24"
   }
 }
@@ -19,14 +19,14 @@ resource "azurerm_virtual_network" "main" {
 resource "azurerm_availability_set" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  name                = "sample-cset"
+  name                = var.availability_set_name
 }
 
 resource "azurerm_virtual_desktop_workspace" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
-  name          = "sample-workspace-c"
+  name          = var.workspace_name
   friendly_name = "FriendlyName"
   description   = "A description of my workspace"
 }
@@ -35,8 +35,8 @@ resource "azurerm_virtual_desktop_host_pool" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
-  name                     = "pooleddepthfirst"
-  friendly_name            = "pooleddepthfirst"
+  name                     = var.host_pool_name
+  friendly_name            = var.host_pool_name
   description              = "Acceptance Test: A pooled host pool - pooleddepthfirst"
   type                     = "Pooled"
   maximum_sessions_allowed = 50
@@ -51,7 +51,7 @@ resource "azurerm_virtual_desktop_application_group" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
-  name          = "remoteappgroup"
+  name          = var.application_group_name
   type          = "RemoteApp"
   host_pool_id  = azurerm_virtual_desktop_host_pool.main.id
   friendly_name = "TestAppGroup"
@@ -61,18 +61,18 @@ resource "azurerm_virtual_desktop_application_group" "main" {
 # Potential crazy shenanigans below
 
 resource "azurerm_key_vault_secret" "hostkey" {
-  name         = "sample-c-hostkey" # "${var.rg_name}-hostkey"
+  name         = lower(replace("${var.resource_group_name}-hostkey", "_", "-"))
   value        = azurerm_virtual_desktop_host_pool.main.registration_info[0].token
   key_vault_id = data.azurerm_key_vault.main.id
 }
 
 resource "azurerm_storage_blob" "main" {
-  name                   = "sample_rg_c" # "${var.rg_name}"
+  name                   = var.resource_group_name
   storage_account_name   = data.azurerm_storage_account.main.name
   storage_container_name = data.azurerm_storage_container.main.name
   type                   = "Block"
   content_type           = "text/plain"
-  source_content         = "sample_rg_c,pooleddepthfirst" # "${var.rg_name},${var.hostpool_name}"
+  source_content         = "${var.resource_group_name},${var.host_pool_name}"
 
   depends_on = [
     azurerm_virtual_desktop_host_pool.main
---
+++ sample_rg_c/infrastructure/terraform/terraform.auto.tfvars
@@ -0,0 +1,7 @@
+resource_group_name    = "SAMPLE_RG_C"
+virtual_network_name   = "virtualNetwork1"
+subnet_name            = "subnet1"
+availability_set_name  = "sample-cset"
+workspace_name         = "sample-workspace-c"
+host_pool_name         = "pooleddepthfirst"
+application_group_name = "remoteappgroup"
